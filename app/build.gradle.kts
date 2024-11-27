d77766af0603768982d7d1247ea4d595f53fb5c1
Rezky Maulana <rezkymaulanaa06@gmail.com>
2024-11-27 23:36:24 +0700

    update: build gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,11 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+
 }
