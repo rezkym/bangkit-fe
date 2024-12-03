f274095a4f451bd539c15f633a9a6290f25d7f07
Rezky Maulana <rezkymaulanaa06@gmail.com>
2024-12-03 19:17:35 +0700

    feat: create onboarding and login screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.annotation)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
